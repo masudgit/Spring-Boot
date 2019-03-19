91573d991beb3aa4ff1192b4ff9dcecb93e8fd13
masudgit <masud.cseu@gmail.com>
2019-03-19 08:49:46 +0600

    updated

--- in28minutes/src/main/java/com/in28minutes/springboot/In28minutesApplication.java
+++ in28minutes/src/main/java/com/in28minutes/springboot/In28minutesApplication.java
@@ -1,15 +1,17 @@
-package com.ikbal.in28minutes;
+package com.in28minutes.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.in28minutes")
 public class In28minutesApplication {
 
 	public static void main(String[] args) {
-		ApplicationContext  applicationContext =  SpringApplication.run(In28minutesApplication.class, args);
-		for(String name : applicationContext.getBeanDefinitionNames()) {
+		ApplicationContext applicationContext = SpringApplication.run(In28minutesApplication.class, args);
+		for (String name : applicationContext.getBeanDefinitionNames()) {
 			System.out.println(name);
 		}
 	}
