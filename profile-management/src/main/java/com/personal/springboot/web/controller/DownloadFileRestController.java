0bf7a8a23cb95e27bca62ad92a514f48597035ae
masud.cseu@gmail.com <masud.cseu@gmail.com>
2019-08-05 16:58:59 +0600

    Jasper example added

--- profile-management/src/main/java/com/personal/springboot/web/controller/DownloadFileRestController.java
+++ profile-management/src/main/java/com/personal/springboot/web/controller/DownloadFileRestController.java
@@ -1,8 +1,13 @@
 package com.personal.springboot.web.controller;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
@@ -14,10 +19,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
 
 //http://localhost:8080/download/pdf/hybrid.pdf
+
 
 @RestController
 @RequestMapping("/download")
@@ -46,4 +60,20 @@ public class DownloadFileRestController {
 		return response;
 
 	}
+
+	@RequestMapping(value = "hello", method = RequestMethod.GET)
+	@ResponseBody
+	public void getReport(HttpServletResponse response) throws JRException, IOException {
+		InputStream jasperStream = this.getClass().getResourceAsStream("/jasperreports/HelloWorld.jasper");
+		Map<String, Object> params = new HashMap<>();
+		JasperReport jasperReport = (JasperReport) JRLoader.loadObject(jasperStream);
+		JasperPrint jasperPrint = JasperFillManager.fillReport(jasperReport, params, new JREmptyDataSource());
+
+		response.setContentType("application/x-pdf");
+		response.setHeader("Content-disposition", "inline; filename=helloWorldReport.pdf");
+
+		final OutputStream outStream = response.getOutputStream();
+		JasperExportManager.exportReportToPdfStream(jasperPrint, outStream);
+	}
+
 }
